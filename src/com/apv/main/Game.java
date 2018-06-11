d2b80a821a87e89221cb70be7b137d02f0ddf41d
2540b98c0838cdc2e96f <2540b98c0838cdc2e96f@10.0.2.6>
2018-06-11 00:42:53 +0000

    Added Graphics

--- src/com/apv/main/Game.java
+++ src/com/apv/main/Game.java
@@ -2,14 +2,19 @@ package com.apv.main;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+
+import com.apv.main.graphics.Screen;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
-	public static int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 
@@ -17,10 +22,16 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	private boolean running = false;
 
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
+		screen = new Screen(width, height);
 		frame = new JFrame();
 	}
 
@@ -41,8 +52,32 @@ public class Game extends Canvas implements Runnable {
 
 	public void run() {
 		while (running == true) {
-
+			update();
+			render();
 		}
+	}
+
+	public void update() {
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		screen.clear();
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
